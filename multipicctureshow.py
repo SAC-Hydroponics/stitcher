325b43bd6ced57fd8be77b3edc281501791d07c1
isaacolson <olsonites@gmail.com>
2018-11-12 19:50:33 -0600

    Update multipicctureshow.py

--- multipicctureshow.py
+++ multipicctureshow.py
@@ -6,14 +6,20 @@ import numpy as np
 image1 = cv2.imread('oneaftertapemeasurer.png',0)
 image2 = cv2.imread('3aftertapemeasurer.png',0)
 
+
 #Translate 2nd image
 rows,cols = image2.shape
 y = int(25/(0.1388))
-M = np.float32([[1,0,0],[0,1,-300]])
+M = np.float32([[1,0,0],[0,1,-y]])
 dst = cv2.warpAffine(image2,M,(cols,rows))
 
 #Add images together
-result = cv2.add(image1, image2)
+result = cv2.add(image1, dst)
+#result = image1 + dst
+
+width, height = result.shape
+print('width = ' + str(width))
+print('height = ' + str(height))
 
 #Display result
 cv2.imshow('result', result)
